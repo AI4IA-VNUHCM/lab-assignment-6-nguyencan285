53f7a37d89845dda383cd07ce14d435e94ea889e
nguyencan285 <91868096+nguyencan285@users.noreply.github.com>
2021-11-22 15:27:44 +0700

    1

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -12,10 +12,34 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-
+#define size 100
 void Ex3(char *str){
 	//Your codes here
-	
+	char words[size][size],min[size],max[size];
+	int i=0,j=0,k,leng;
+	for(k=0;str[k]!='\0';k++)
+	{
+		if(str[k]!= ' '&& str[k]!='\0')
+		{
+			words[i][j++]=str[k];
+		}
+		else
+		words[i][j]='\0';
+		i++;
+		j=0;
+	}
+	leng=i+1;
+	strcpy(min,words[0]);
+	strcpy(min,words[0]);
+	for (k=0;k<leng;k++)
+	{
+		if(strlen(min)>strlen(words[k]))
+		strcpy(min,words[k]);
+		if(strlen(max)<strlen(words[k]))
+		strcpy(max,words[k]);
+	}
+	printf("Shortest word: %s\n",min);
+	printf("Longest word: %s",max);
 }
 
 int main(int argc, char *argv[]) {
