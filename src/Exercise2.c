f310e4fc42852a54035f7f67bd45904101b3556b
nguyencan285 <91868096+nguyencan285@users.noreply.github.com>
2021-11-22 18:25:42 +0700

    1

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -19,14 +19,15 @@ void Ex2(int n, char *str[]){
 	for (i = 0; i < n; ++i) {
         for (j = i + 1; j < n; ++j) {
             
-            if ( strcmp(str[i], str[j]) > 0) {
+            if ( strcmp(str[i], str[j]) > 0) 
+				{
                 strcpy(temp, str[i]);
                 strcpy(str[i], str[j]);
                 strcpy(str[j], temp);
                 }
         }
     }
-	
+	for(i=0;i<n;i++)
          printf("%s ",str[i]);
 	
 }
