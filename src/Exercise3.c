87b05c749cb4a5f56df6a778d161c0a17b8b3bd7
nguyencan285 <91868096+nguyencan285@users.noreply.github.com>
2021-11-22 17:57:09 +0700

    1

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -14,37 +14,42 @@ Ex:
 #include <string.h>
 
 
-void Ex3(char *str){
+//void Ex3(char *str){
 	//Your codes here
-	char words[1000][1000],min[1000],max[1000];
-	int i=0,j=0,k,leng;
-	for(k=0;str[k]!='\0';k++)
-	{
-		if(str[k]!= ' '&& str[k]!='\0')
-		{
-			words[i][j++]=str[k];
-		}
-		else
-		words[i][j]='\0';
-		i++;
-		j=0;
-	}
-	leng=i+1;
-	strcpy(min,words[0]);
-	strcpy(max,words[0]);
-	for (k=0;k<leng;k++)
-	{
-		if(strlen(min)>strlen(words[k]))
-		strcpy(min,words[k]);
-		if(strlen(max)<strlen(words[k]))
-		strcpy(max,words[k]);
-	}
-	printf("Shortest word: %s\n",min);
-	printf("Longest word: %s",max);
-}
+//}
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	char *testcase = argv[1];
-	Ex3(testcase);
+	char words[100][100], min[100], max[100];
+	int i = 0, j = 0, k, len;
+	for(k=0; testcase[k]!='\0'; k++)
+    {
+        if(testcase[k] != ' ' && testcase[k] != '\0')
+		{
+            words[i][j++] = testcase[k];
+        }
+        else
+		{
+            words[i][j] = '\0';
+			i++;
+			j = 0;
+			 }
+    }
+    
+    len = i + 1;
+	strcpy(min, words[0]);
+	strcpy(max, words[0]); 
+	for(k = 0; k < len; k++)
+	{
+    if(strlen(min) > strlen(words[k]))
+	strcpy(min, words[k]); 
+	if(strlen(max) < strlen(words[k])) 
+	strcpy(max, words[k]);
+	}
+    printf("Shortest word: %s\n", min);
+    printf("Longest word: %s", max);
+      
+
+	//Ex3(testcase);
 	return 0;
 }
